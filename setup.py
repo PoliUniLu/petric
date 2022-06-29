260861df8b1d82f0f6a7e34abc9864d1630dbff9
ZuzanaSebb <69768202+ZuzanaSebb@users.noreply.github.com>
2022-06-29 09:42:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ module = Extension('native_petric',
                    extra_compile_args=['-std=c++14'])
 
 setup(name = 'NativePetric',
-      version = 0.2,
+      version = "0.2",
       description = 'Native implementation test',
       ext_modules = [module],
       test_suite='tests')
